8d1eefc9bbb9f50dd8c273c3a53da2aaef5f2b0a
Fauzi-kun <fauzimalik08@gmail.com>
2025-04-14 05:49:11 +0700

    feat: add database

--- main.go
+++ main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var notes = []Note{
 	{ID: 1, Title: "Belajar Go", Content: "Pelajari Gin dan GORM"},
 }
+var db *gorm.DB
 
 func main() {
+	initDB()
 	r := gin.Default()
 
-	r.GET("/notes",func(c *gin.Context){
-		c.JSON(http.StatusOK, notes)
-	})
+	r.GET("/notes",get)
 	r.POST("/notes",post)
 	r.PUT("/notes/:id",put)
 	r.DELETE("/notes/:id",delete)
 	r.Run(":8080")
+}
+func get(c *gin.Context){
+	var allNotes []Note
+	db.Find(&allNotes)
+	c.JSON(http.StatusOK, allNotes)
 }
 func post(c *gin.Context){
 	var newNote Note
@@ -28,45 +34,50 @@ func post(c *gin.Context){
 		c.JSON(http.StatusBadRequest,gin.H{"error": err.Error()})
 		return
 	} 
-	newNote.ID = len(notes) + 1
-	notes = append(notes, newNote)
+	db.Create(&newNote)
 	c.JSON(http.StatusCreated,newNote)
 }
 func put(c *gin.Context){
-	idParam := c.Param("id")
-	id, err :=strconv.Atoi(idParam)
-	if err != nil{
-		c.JSON(http.StatusBadRequest,gin.H{"error": "ID nggak valid!"})
+	id := c.Param("id")
+	var note Note
+
+	//cari note
+	if err := db.First(&note,id).Error; err != nil{
+		c.JSON(http.StatusNotFound,gin.H{"error": "Note nggak ditemukan!"})
 		return
 	}
-	var updatedNote Note
-	if err := c.BindJSON(&updatedNote); err != nil{
+
+	//bin data baru
+	var updatedData Note
+	if err := c.BindJSON(&updatedData); err != nil{
 		c.JSON(http.StatusBadRequest,gin.H{"error": "Format JSON salah!"})
 		return
 	}
-	for i, note := range notes{
-		if note.ID == id{
-			notes[i].Title = updatedNote.Title
-			notes[i].Content = updatedNote.Content
-			c.JSON(http.StatusOK,notes[i])
-			return
-		}
-	}
-	c.JSON(http.StatusNotFound,gin.H{"error": "Note nggak ditemukan!"})
+	
+	//update field
+	note.Title = updatedData.Title
+	note.Content = updatedData.Content
+	db.Save(&note)
+	c.JSON(http.StatusOK,note)
 }
 func delete(c *gin.Context){
-	idParam := c.Param("id")
-	id, err := strconv.Atoi(idParam)
-	if err != nil{
-		c.JSON(http.StatusBadRequest,gin.H{"error": "ID nggak valid!"})
+	id := c.Param("id")
+	var note Note
+
+	//Cari note
+	if err := db.First(&note,id).Error; err != nil{
+		c.JSON(http.StatusNotFound,gin.H{"error": "Note tidak ditemukan!"})
 		return
 	}
-	for i, note := range notes{
-		if note.ID == id{
-			notes = append(notes[:i], notes[i+1:]...)
-			c.JSON(http.StatusOK,gin.H{"message": "Note berhasil dihapus!"})
-			return
-		}
+	db.Delete(&note)
+	c.JSON(http.StatusOK,gin.H{"Message": "Note berhasil dihapus!"})
+}
+func initDB(){
+	dsn := "host=localhost user=postgres password=fauzi dbname=notes_db port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	if err != nil{
+		panic("Gagal koneksi ke database")
 	}
-	c.JSON(http.StatusNotFound,gin.H{"error": "Note nggak ditemukan!"})
+	db = database
+	db.AutoMigrate(&Note{})
 }
